ad75b2f1ceb7d7d1ea1606712ff8c9c56610564d
Patrick Gilhooley <113308245+pgil256@users.noreply.github.com>
2023-09-05 16:06:36 -0400

    Delete src directory

--- src/auth/UserContext.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-//Creates context for user in app; renders html and fetches data based on specific user credentials
-const UserContext = React.createContext();
-
-export default UserContext;
--- src/common/LoadingSpinner.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-//Component that renders during API requests are loading
-function LoadingSpinner() {
-  return <div className='LoadingSpinner'>Loading ...</div>;
-}
-
-export default LoadingSpinner;
